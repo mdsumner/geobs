a4b152f5cd53a8ce9a7af8dba9d7d51719ef86b1
Michael Sumner <mdsumner@gmail.com>
2023-03-07 07:11:31 +1100

    use current version

--- R/get_bounds.R
+++ R/get_bounds.R
@@ -1,8 +1,10 @@
-#' Get geoboundaries in a dataframe from GeoJSON with a code
+#' Get geoboundaries in a dataframe from zipped geoboundaries with a code and level.
 #'
-#' Currently hardcoded to ADM1, see https://www.geoboundaries.org/api.html
+#' Currently hardcoded to gbOpen, see https://github.com/wmgeolab/geoBoundaries/tree/main/releaseData/gbOpen
 #'
 #' There's no caching done, but the function get_bounds is memoized.
+#' See examples for available codes.
+#'
 #' @param code 3-letter code
 #'
 #' @return data frame with fields and geometry (see [wk::wkb])
@@ -13,9 +15,21 @@
 #' @importFrom memoise memoize
 #' @examples
 #' get_bounds("EGY")
-get_bounds <- function(code = "AUS") {
-  f <- sprintf("/vsizip//vsicurl/https://geoboundaries.org/data/geoBoundaries-3_0_0/%s/ADM1/geoBoundaries-3_0_0-%s-ADM1-all.zip/geoBoundaries-3_0_0-%s-ADM1.geojson", code, code, code)
-  d <- tibble::as_tibble(vapour_read_fields(f))
-  d$geometry <- wk::wkb(vapour_read_geometry(f))
+#'
+#' ## get a table of codes and levels
+#' codes <- readr::read_csv("https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/geoBoundariesOpen-meta.csv")
+#' subset(codes, boundaryISO == "SWE", select = c("boundaryISO", "boundaryName", "boundaryType"))
+get_bounds <- function(code = "AUS", level = "ADM1", simplified = FALSE) {
+  f <- sprintf("/vsizip//vsicurl/https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/gbOpen/%s/%s/geoBoundaries-%s-%s-all.zip", code, level, code, level)
+  ## layers are
+  #geoBoundaries-SWE-ADM1 (Polygon)
+  #geoBoundaries-SWE-ADM1_simplified
+  simplif <- ""
+  if (simplified) {
+    simplif <- "_simplified"
+  }
+  layer <- sprintf("geoBoundaries-%s-%s%s", code, level, simplif)
+  d <- tibble::as_tibble(vapour_read_fields(f, layer))
+  d$geometry <- wk::wkb(vapour_read_geometry(f, layer))
   d
 }
