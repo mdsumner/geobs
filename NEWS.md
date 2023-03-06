0fe5ff50c67cae182ab791e4863a14ed927eab35
Michael Sumner <mdsumner@gmail.com>
2023-03-07 07:41:08 +1100

    via geojson, optionally suppress data return and print

---
+++ NEWS.md
@@ -0,0 +1,13 @@
+# geobs 0.0.1
+
+* Additions to `get_bounds()`. 
+
+Return the data or dsn/layer with 'data' arg. 
+
+Suppress debug information about the dsn/layer with 'quiet'. 
+
+Return data from the GeoJSON with 'geojson' arg. 
+
+* Improved version of `get_bounds()` to use the github releases, return code, level, simplified or native to the level. 
+
+* First version `get_bounds()`. Was tied to geoboundaries version 3. 
--- R/get_bounds.R
+++ R/get_bounds.R
@@ -8,6 +8,9 @@
 #' @param code 3-letter code
 #' @param level administrative level (of detail, e.g. "ADM1")
 #' @param simplified return the simplified layer for the level, `FALSE` is default
+#' @param geojson use the GeoJSON source (not the SHP shapefile which is default), `FALSE` by default
+#' @param quiet simple debug messages are printed if this is `TRUE`
+#' @param data return the data, or just the source and layer name, default is `TRUE`
 #'
 #' @return data frame with fields and geometry (see [wk::wkb])
 #' @export
@@ -19,9 +22,10 @@
 #' get_bounds("EGY", simplified = TRUE)
 #'
 #' ## get a table of codes and levels
-#' codes <- readr::read_csv("https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/geoBoundariesOpen-meta.csv")
+#' b <- "https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData"
+#' codes <- readr::read_csv(file.path(b, "geoBoundariesOpen-meta.csv"))
 #' subset(codes, boundaryISO == "SWE", select = c("boundaryISO", "boundaryName", "boundaryType"))
-get_bounds <- function(code = "AUS", level = "ADM1", simplified = FALSE) {
+get_bounds <- function(code = "AUS", level = "ADM1", simplified = FALSE, geojson = FALSE,  quiet = TRUE, data = TRUE) {
   f <- sprintf("/vsizip//vsicurl/https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/gbOpen/%s/%s/geoBoundaries-%s-%s-all.zip", code, level, code, level)
   ## layers are
   #geoBoundaries-SWE-ADM1 (Polygon)
@@ -31,6 +35,12 @@ get_bounds <- function(code = "AUS", level = "ADM1", simplified = FALSE) {
     simplif <- "_simplified"
   }
   layer <- sprintf("geoBoundaries-%s-%s%s", code, level, simplif)
+  if (geojson) f <- file.path(f, sprintf("%s.geojson", layer))
+  if (!quiet) print(str(vapour::vapour_layer_info(f, layer)))
+  if (!data) {
+    return(tibble::tibble(dsn = f,
+             layer = layer))
+  }
   d <- tibble::as_tibble(vapour_read_fields(f, layer))
   d$geometry <- wk::wkb(vapour_read_geometry(f, layer))
   d
