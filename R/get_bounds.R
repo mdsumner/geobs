9e95b8dcf40838b4a3d5eb9a9fd65af66eaadfc8
Michael Sumner <mdsumner@gmail.com>
2023-03-06 22:31:33 +1100

    Initial commit

---
+++ R/get_bounds.R
@@ -0,0 +1,18 @@
+#' Get geoboundaries in a dataframe from GeoJSON with a code
+#'
+#' @param code 3-letter code
+#'
+#' @return
+#' @export
+#' @importFrom tibble as_tibble
+#' @importFrom vapour vapour_read_fields vapour_read_geometry
+#' @importFrom wk wkb
+#' @importFrom
+#' @examples
+#' get_bounds("EGY")
+get_bounds <- function(code = "AUS") {
+  f <- sprintf("/vsizip//vsicurl/https://geoboundaries.org/data/geoBoundaries-3_0_0/%s/ADM1/geoBoundaries-3_0_0-%s-ADM1-all.zip/geoBoundaries-3_0_0-%s-ADM1.geojson", code, code, code)
+  d <- tibble::as_tibble(vapour_read_fields(f))
+  d$geometry <- wk::wkb(vapour_read_geometry(f))
+  d
+}
